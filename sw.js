334397d549781c08fa40d0724b131fe7e193fa13
emmasolucionesdigitales-lang <emmasolucionesdigitales@gmail.com>
2026-04-26 18:19:17 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'admin-emma-v1';
+
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() {
+    return caches.match(e.request);
+  }));
+});
